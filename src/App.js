e3e140f7aa9eac8a9258a5e1a9ae8bb844d18591
AD115 <ad115@MacBook-Pro-de-AD115.local>
2023-02-24 22:20:45 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Accueil from "./components/Accueil";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Instrument from "./components/Instrument";
+import SejourFab from "./components/SejourFab";
+import SeanceIndiv from "./components/SeanceIndiv";
+import SeanceMassage from "./components/SeanceMassage";
+import Contact from "./components/Contact";
+import Nettoyage from "./components/Nettoyage";
+import BackToTopButton from "./components/BackToTopButton";
 
 function App() {
+
+ 
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/tambours_chamanique_et_hochets"
+            element={<Instrument />}
+          />
+          <Route
+            path="/sejour_fabrication_tambour_chamanique"
+            element={<SejourFab />}
+          />
+          <Route
+            path="/seance_individuelle_chamanique"
+            element={<SeanceIndiv />}
+          />
+          <Route
+            path="/seance_individuelle_massage_par_le_magnetisme"
+            element={<SeanceMassage />}
+          />
+          <Route
+            path="/nettoyage_et_harmonisation_lieu_de_vie"
+            element={<Nettoyage />}
+          />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <BackToTopButton/>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
